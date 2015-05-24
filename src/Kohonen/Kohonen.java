d85e99b289a95c27da9c9d9eb3a0e0a05375c487
Axel Fahy <axel@fahy.net>
2015-05-24 18:56:14 +0200

    Initial commit

---
+++ src/Kohonen/Kohonen.java
@@ -0,0 +1,12 @@
+package Kohonen;
+
+/**
+ * Created by axel on 5/24/15.
+ */
+public class Kohonen {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
